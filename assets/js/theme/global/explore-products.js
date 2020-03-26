ab5c942f08663ab6c1bd98bf0b6ba6263ed3b3ce
Ken Barrios <keniswaiting@gmail.com>
2020-03-26 17:43:41 -0500

    got stuck on jQuery, it trips me up so moving over to vanilla JS

--- assets/js/theme/global/explore-products.js
+++ assets/js/theme/global/explore-products.js
@@ -1,8 +1,39 @@
 export default function () {
-	let $headerNavContainer = $('.header-nav-container');
-	$(document).on('click','#exploreProducts', (event) => {
-		event.preventDefault();
-		console.log();
-        $headerNavContainer.toggleClass('active-sub-nav');
-    });
+
+	document.addEventListener('click', (event) => {
+		let exploreProducts = document.getElementById('exploreProducts');
+		let headerNavContainer = document.getElementsByClassName('header-nav-container');
+
+		if (exploreProducts.contains(event.target)) {
+			let headerClassList = headerNavContainer[0].classList
+
+			if ( headerClassList.contains('active-sub-nav') ) {
+				return headerClassList.remove('active-sub-nav');
+			}
+			return headerClassList.add('active-sub-nav');
+		}
+
+
+	});
+
+	// $(document).on('click', '#exploreProducts', function(event) {
+
+	// 	let $headerNavContainer = $('.header-nav-container');
+	// 	console.log('test ', event);
+	// 	if ( $headerNavContainer.hasClass('active-sub-nav') ) {
+	// 		console.log('has class');
+	// 		return $headerNavContainer.removeClass('active-sub-nav');
+	// 	}
+
+	// 	return $headerNavContainer.addClass('active-sub-nav');
+	// });
+
+	// $(document).click(function(e) {
+	// 	let $lowerLevelSubNav = $('.lower-level-sub-nav');
+	// 	let $exploreProducts = $('#exploreProducts');
+	// 	// if the target of the click isn't the container nor a descendant of the container
+	// 	if (!$lowerLevelSubNav.is(e.target) && $lowerLevelSubNav.has(e.target).length === 0) {
+	// 		$('.header-nav-container').removeClass('active-sub-nav');
+	// 	}
+	// });
 }
